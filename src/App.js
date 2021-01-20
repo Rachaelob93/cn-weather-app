47af16220ff87fc5a73b494bbf9a0426ebd0e295
Rachael O'Brien <rachaelob93@gmail.com>
2021-01-20 16:19:55 +0000

    initial css and app.js changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import React, {useState, useEffect} from "react";
 import './App.css';
+import LocationSearch from "./components/LocationSearch";
+import Heading from "./components/Heading";
+// import WeatherAPI from "./WeatherAPI";
 
-function App() {
-  return (
-    <div className="App">
+
+class App extends React.Component {
+
+  // const [city, setCity] = React.useState(null);
 
 
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        {/* <TextField autoFocus label="City Name" 
+          onChange={(e)=> {setCity(e.target.value); }}
+        />
+          <WeatherAPI city={city} /> */}
+          <LocationSearch />
+          <Heading />
+         
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/LocationSearch.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../App.css"
+
+const TextField = () =>{
+   return <div className="searchElementDiv">
+            <input className="location-input" type="text" placeholder="Location"/>
+            <button className="btn">Check Weather</button>
+        </div>
+       
+}
+
+
+
+
+
+export default TextField;
