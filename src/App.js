6c1842e97373c6755ad12d055f85c4ad98637c9e
Peippo <32274695+Peippo1@users.noreply.github.com>
2021-01-21 10:41:50 +0000

    display commit 2

--- src/App.js
+++ src/App.js
@@ -4,22 +4,25 @@ import LocationSearch from "./components/LocationSearch";
 import Heading from "./components/Heading";
 // import WeatherAPI from "./WeatherAPI";
 
+// 1. Set up your new Create react app. (DONE)
+// 2. Render a ‘starter screen’, hello world or something, just get it on screen. (DONE - kinda)
+
+// 3. Create a state value (hooks or state = {} w/ class component) and then render than (display it on the screen). 
+// 4. Fetch your api and console log it (try this in a function that you call on a button click - forget about useEffect/compDidMount for now)
+// 5. Now try putting that data into the state from the function.
+// 6. Display the data correctly from the state
+// 7. Set up an input and store it in a state value
+// 8. Add the input to the api fetch string so the api now returns the correct weather for the input
+
 
 class App extends React.Component {
-
-  // const [city, setCity] = React.useState(null);
 
 
   render() {
     return (
       <div>
-        {/* <TextField autoFocus label="City Name" 
-          onChange={(e)=> {setCity(e.target.value); }}
-        />
-          <WeatherAPI city={city} /> */}
           <LocationSearch />
-          <Heading />
-         
+          <Heading />        
       </div>
     )
   }
--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./weatherAPI.css"; 
 
+import Display from "./weatherReportDisplay"
+
 const API_KEY = process.env.REACT_APP_API_KEY
 const UNITS = "Metric"
 const LANG = "en"
@@ -15,7 +17,7 @@ class WeatherAPI extends React.Component {
         }
     };
    componentDidUpdate(){
-       let URL = /*api url*/ +this.props.city +"&lang=" + LANG + "&appid=" + API_KEY + "&units="+ UNITS;
+       let URL = "http://api.openweathermap.org/data/2.5/forecast?id&lang=" + LANG + "&appid=" + API_KEY + "&units="+ UNITS;
        fetch(URL).then(response =>{
            if(response.ok) {return response.json()}
            else {throw new Error("Something went wrong")}}) /*error?*/
@@ -24,22 +26,23 @@ class WeatherAPI extends React.Component {
                 isLoading: false}))
             .catch(error => this.setState( {error, isLoading : true }));
    } 
+
    render () {
        if(this.state.isLoading) {
-        if(this.props.city !=null) {
-            return (
-                <div>
-                    {/* input to be commited */}
-                </div>
+            if(this.props.city !=null) {
+                return (
+                    <div>
+                        {/* input to be commited */}
+                    </div>
+                )
+            }
+            else return null;
+       }
+        else {
+            return(
+                <Display weatherReport = {this.state.weatherReport}/>
             )
         }
-        else return null;
-       }
-    else {
-        return(
-            <Display weatherReport = {this.state.weatherREport}/>
-        )
-    }
    }
 };
 
